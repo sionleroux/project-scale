2b65154736056f8059c4aef31d782808027eb65e
asymetrique <peter.kertesz@outlook.com>
2023-11-25 15:00:15 +0100

    Parallax effect using backdrop scaling and speed

--- backdrops.go
+++ backdrops.go
@@ -13,16 +13,16 @@ func NewBackdrops(bottomOfMap float64) Backdrops {
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0014_Sky.png"), false, 0.0},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0013_Smog-5.png"), false, 0.0},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0012_Water-5.png"), true, 0.0},
-			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0011_City-4.png"), false, 0.0},
+			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0011_City-4.png"), false, 0.5},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0010_Smog-4.png"), false, 0.0},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0009_Water-4.png"), true, 0.0},
-			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0008_City-3.png"), false, 0.0},
+			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0008_City-3.png"), false, 0.35},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0007_Smog-3.png"), false, 0.0},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0006_Water-3.png"), true, 0.0},
-			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0005_City-2.png"), false, 0.0},
+			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0005_City-2.png"), false, 0.2},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0004_Smog-2.png"), false, 0.0},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0003_Water-2.png"), true, 0.0},
-			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0002_City-1.png"), false, 0.0},
+			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0002_City-1.png"), false, 0.05},
 			{loadImage("assets/backdrop/Project-scale-parallax-backdrop_0001_Smog-1.png"), false, 0.0},
 		},
 	}
@@ -39,12 +39,6 @@ func (bs Backdrops) Draw(cam *camera.Camera, waterLevel float64) {
 	backdropCenter := -float64(bs.Backdrops[0].Image.Bounds().Dx()) / 2
 	watersDone := 0.0
 
-	backdropPos := cam.GetTranslation(
-		&ebiten.DrawImageOptions{},
-		backdropCenter,
-		-float64(bs.Backdrops[0].Image.Bounds().Dy())+bs.bottomOfMap,
-	)
-
 	for _, b := range bs.Backdrops {
 		if b.Water {
 			cam.Surface.DrawImage(b.Image, cam.GetTranslation(
@@ -54,6 +48,26 @@ func (bs Backdrops) Draw(cam *camera.Camera, waterLevel float64) {
 			))
 			watersDone++
 		} else {
+
+			// Due to parallax effect, the further the backdrop is the slower it moves.
+			// Thus in order to reach the top of all the backdrops at the top,
+			// they needs to be scaled down according to their speed
+
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(-float64(b.Image.Bounds().Dx())/2, 0)
+			op.GeoM.Scale(1-b.Speed, 1-b.Speed)
+			op.GeoM.Translate(float64(b.Image.Bounds().Dx())/2, 0)
+			backdropPos := cam.GetTranslation(
+				op,
+				// Use the actual horizontal position of the backgrop, and
+				// change it based on the camera position and the speed of the backdrop
+				-float64(b.Image.Bounds().Dx())/2*(1-b.Speed)+(cam.X-float64(cam.Width/2))*b.Speed,
+				// Use the actual height of the backdrop after scaling it down
+				-float64(b.Image.Bounds().Dy())*(1-b.Speed)+bs.bottomOfMap+
+					// The position is changed based on the camera position and the speed of the backdrop
+					(float64(cam.Height/2)+cam.Y-bs.bottomOfMap)*b.Speed,
+			)
+
 			cam.Surface.DrawImage(b.Image, backdropPos)
 		}
 	}
